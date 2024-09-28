defdc176df976526673e02f43a4c132aaad2ae32
cccaternberg <acaternberg@cloudbees.com>
2024-09-28 19:23:20 +0200

    reorganize

--- env.sh
+++ env.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 
-# Hostnames for Operations Center and Controllers
-# The controllers are in HA mode, listening on a single CLIENTS_URL
-# HAProxy listens for this URL and load balances between the controllers
-export OC_URL=oc.ha
-export CLIENTS_URL=client.ha
+set -x
+
+#### Docker image settings
 
 # CB CI version for Operations Center and Controllers
-#export DOCKER_IMAGE_OC=cloudbees/cloudbees-core-oc:2.426.3.3
 export DOCKER_IMAGE_OC=cloudbees/cloudbees-core-oc:latest
-#export DOCKER_IMAGE_CLIENT=cloudbees/cloudbees-core-cm:2.426.3.3
 export DOCKER_IMAGE_CLIENT=cloudbees/cloudbees-core-cm:latest
+# SSH AGENT see https://hub.docker.com/r/jenkins/ssh-agent
+export DOCKER_IMAGE_JENKINS_SSH_AGENT=jenkins/ssh-agent:jdk17
+export DOCKER_IMAGE_HAPROXY=haproxy:alpine
+# see https://docs.linuxserver.io/images/docker-webtop/#version-tags
+export DOCKER_IMAGE_BROWSER_BOX=lscr.io/linuxserver/webtop:latest
+
+########################################################################################################################
+
+#### Docker network settings
 
 #### We put static IP addresses for docker containers
 export IP_PREFIX=172.47
@@ -21,31 +26,63 @@ export CLIENT2_IP=$IP_PREFIX.0.8
 export AGENT_IP=$IP_PREFIX.0.9
 export BROWSER_IP=$IP_PREFIX.0.10
 
+########################################################################################################################
+
+#### DNS/URL settings
+
+# Hostnames for Operations Center and Controllers
+# The controllers are in HA mode, listening on a single CLIENTS_URL
+# HAProxy listens for this URL and load balances between the controllers
+export OC_URL=oc.ha
+export CLIENTS_URL=client.ha
+
+########################################################################################################################
+
+#### Docker host volume settings
+
 #### Paths on Docker host for mapped volumes
-export PERSISTENCE_PREFIX=$(pwd)/jenkins_ha
+export PERSISTENCE_PREFIX=$(pwd)/cloudbees_ci_ha_volumes
 export BROWSER_PERSISTENCE=$PERSISTENCE_PREFIX/browser
 export OC_PERSISTENCE=$PERSISTENCE_PREFIX/oc
 export CONTROLLER_PERSISTENCE=$PERSISTENCE_PREFIX/controllers
 export CONTROLLER1_CACHES=$PERSISTENCE_PREFIX/controller1_caches
-#export CONTROLLER1_PLUGINS_CACHE=$PERSISTENCE_PREFIX/controller1_plugins
 export CONTROLLER2_CACHES=$PERSISTENCE_PREFIX/controller2_caches
-#export CONTROLLER2_PLUGINS_CACHE=$PERSISTENCE_PREFIX/controller2_plugins
 export AGENT_PERSISTENCE=$PERSISTENCE_PREFIX/ssh-agent1
 
+########################################################################################################################
+
 #### Controller settings
+
 #https://docs.cloudbees.com/docs/cloudbees-ci/latest/ha/specific-ha-installation-traditional#_jenkins_args
+#For Docker we must set JENKINS_OPTS instead of JENKINS_ARGS
 export CONTROLLER_JENKINS_OPTS="--webroot=/var/cache/cloudbees-core-cm/war --pluginroot=/var/cache/cloudbees-core-cm/plugins"
 #https://docs.cloudbees.com/docs/cloudbees-ci/latest/ha/specific-ha-installation-traditional#_java_options
 export CONTROLLER_JAVA_OPTS="--add-exports=java.base/jdk.internal.ref=ALL-UNNAMED --add-modules=java.se --add-opens=java.base/java.lang=ALL-UNNAMED --add-opens=java.base/sun.nio.ch=ALL-UNNAMED --add-opens=java.management/sun.management=ALL-UNNAMED --add-opens=jdk.management/com.sun.management.internal=ALL-UNNAMED -Djenkins.model.Jenkins.crumbIssuerProxyCompatibility=true -DexecutableWar.jetty.disableCustomSessionIdCookieName=true -Dcom.cloudbees.jenkins.ha=false -Dcom.cloudbees.jenkins.replication.warhead.ReplicationServletListener.enabled=true -Djenkins.plugins.git.AbstractGitSCMSource.cacheRootDir=/var/cache/cloudbees-core-cm/caches/git -Dorg.jenkinsci.plugins.github_branch_source.GitHubSCMSource.cacheRootDir=/var/cache/cloudbees-core-cm/caches/github-branch-source -XX:+AlwaysPreTouch -XX:+UseStringDeduplication -XX:+ParallelRefProcEnabled -XX:+DisableExplicitGC"
+# https://docs.cloudbees.com/docs/cloudbees-ci/latest/casc-oc/configure-oc-traditional#_adding_the_java_system_property
+# We assign the controller casc bundle directly, comment out if you don't want to use casc
+export CONTROLLER_JAVA_OPTS="$CONTROLLER_JAVA_OPTS -Dcore.casc.config.bundle=/var/jenkins_home/cascbundle"
 
-### Operatins center setting
+########################################################################################################################
+
+#### Operations center setting
 export CJOC_JAVA_OPTS="-XX:+AlwaysPreTouch -XX:+UseStringDeduplication -XX:+ParallelRefProcEnabled -XX:+DisableExplicitGC"
+# https://docs.cloudbees.com/docs/cloudbees-ci/latest/casc-oc/configure-oc-traditional#_adding_the_java_system_property
+# We assign the cjoc casc bundle, comment out if you don't want to use casc
+export CJOC_JAVA_OPTS="$CJOC_JAVA_OPTS -Dcore.casc.config.bundle=/var/jenkins_home/cascbundle"
+# Cjoc login user and password
+export CJOC_LOGIN_USER="admin"
+export CJOC_LOGIN_PW="admin"
 
+########################################################################################################################
 
 #### Agent settings
-#https://hub.docker.com/r/jenkins/ssh-agent
+#see https://hub.docker.com/r/jenkins/ssh-agent
+# To use this image with Docker Plugin⁠, you need to pass the public SSH key using environment variable JENKINS_AGENT_SSH_PUBKEY and not as a startup argument.
+# In Environment field of the Docker Template (advanced section), just add:
 #export JENKINS_AGENT_SSH_PUBKEY="ssh-rsa AD_YOUR_JENKINS_AGENT_SSH_PUBKEY"
 export JENKINS_AGENT_SSH_PUBKEY=$(cat ~/.ssh/id_rsa.pub)
+# used in casc controller bundle to initialize the ssh-agent credential
+export JENKINS_AGENT_SSH_PRIVKEY=$(cat ~/.ssh/id_rsa)
 
 
 
