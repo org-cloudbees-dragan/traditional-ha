dcff97e7917f408cb96f95d32273635c261f83ec
cccaternberg <acaternberg@cloudbees.com>
2024-10-01 17:07:00 +0200

    add

---
+++ test/README.md
@@ -0,0 +1,44 @@
+# Test for demo
+
+## Test Pipeline 
+
+[Jenkinsfile-ssh-agent.groovy](Jenkinsfile-ssh-agent.groovy) is a simple test pipeline that prints out the agent host name as wel as the controller replica hostname
+
+## Test script 
+
+Running the [testJobTrigger.sh](testJobTrigger.sh]) will trigger the Test Pipeline job in a loop by curl
+Curl doesnt send the cookie so that each Job trigger request will get balanced by HAProxy to the Controller with less workload available. 
+
+
+
+You need to create an ADMIN TOKEN on the Operations Center first.
+
+Run the script:
+
+> ./testJobTrigger.sh <ADMIN_TOKEN>
+
+output:  (You can se each request  passes another controller replica) 
+
+```
+#######################################
+start build of Job: testpipeline
+LOCATION:                 http://client.ha/queue/item/1661113525441582349/
+CONTROLLER_REPLICA:       8f09fe15eb03@7
+CONTROLLER_REPLICA_IP:    172.47.0.7
+#######################################
+start build of Job: testpipeline
+LOCATION:                 http://client.ha/queue/item/1042037895850911901/
+CONTROLLER_REPLICA:       ca6d130f20e4@6
+CONTROLLER_REPLICA_IP:    172.47.0.8
+#######################################
+start build of Job: testpipeline
+LOCATION:                 http://client.ha/queue/item/724259527559776060/
+CONTROLLER_REPLICA:       8f09fe15eb03@7
+CONTROLLER_REPLICA_IP:    172.47.0.7
+#######################################
+start build of Job: testpipeline
+LOCATION:                 http://client.ha/queue/item/5229488099980267107/
+CONTROLLER_REPLICA:       ca6d130f20e4@6
+CONTROLLER_REPLICA_IP:    172.47.0.8
+...
+```
--- test/testJobTrigger.sh
+++ test/testJobTrigger.sh
@@ -2,9 +2,14 @@
 
 source ../env.sh
 
+
+
+#see curl headers https://daniel.haxx.se/blog/2022/03/24/easier-header-picking-with-curl/
+
 echo "YOU NEED TO CREATE A ADMIN TOKEN ON THE OPERATIONS CENTER FIRST"
-#ADMIN_USER and ADMIN_TOKEN combined
-ADMINTOKEN="admin:<ADMINTOKEN>"
+
+JENKINS_TOKEN=${1:-"YOUR_ADMIN_TOKEN"}
+JENKINS_USER_TOKEN="admin:$JENKINS_TOKEN"
 
 #CONTROLLER_URL=http://client.ha
 CONTROLLER_URL=http://${CLIENTS_URL}
@@ -21,13 +26,9 @@ do
  	echo "#######################################"
 	echo "start build of Job: $JOB"
 
-	#curl -i -u $TOKEN -X POST  $CONTROLLER_URL/job/$JOB/build
-	#RESPONSE=$(curl  -si -w "\n%{size_header},%{size_download}" -u $TOKEN -X POST  $CONTROLLER_URL/job/$JOB/build)
-	#see curl headers https://daniel.haxx.se/blog/2022/03/24/easier-header-picking-with-curl/
-  #-b cookie-jar.txt
 
   # trigger our testpipeline
-	curl --connect-timeout  $CONNECT_TIMEOUT  -s -IL -o $RESPONSEHEADERS  -u $ADMINTOKEN -X POST  "$CONTROLLER_URL/job/$JOB/build?delay=0sec"
+	curl --connect-timeout  $CONNECT_TIMEOUT  -s -IL -o $RESPONSEHEADERS  -u $JENKINS_USER_TOKEN -X POST  "$CONTROLLER_URL/job/$JOB/build?delay=0sec"
   # check if we got a healthy HTTP response state in the response header
   # Response header gets written by each loop/request in the $RESPONSEHEADERS (heade) file
   if [ -z "$(cat $RESPONSEHEADERS |grep -oE 'HTTP/2 201|HTTP/ 200|HTTP/1.1 201')" ]
@@ -39,9 +40,9 @@ do
 	    LOCATION="$(cat $RESPONSEHEADERS    |grep -E "location.*"                  |  awk '{print $2}')"
 	    REPLICA="$(cat $RESPONSEHEADERS     |grep -E "x-jenkins-replica-host.*"    |  awk '{print $2}')"
 	    REPLICA_IP="$(cat $RESPONSEHEADERS  |grep -E "x-jenkins-replica-address.*" |  awk '{print $2}')"
-	    echo "LOCATION:  $LOCATION"
-	    echo "REPLICA:   $REPLICA"
-	    echo "REPLICA:   $REPLICA_IP"
+	    echo "LOCATION:                 $LOCATION"
+	    echo "CONTROLLER_REPLICA:       $REPLICA"
+	    echo "CONTROLLER_REPLICA_IP:    $REPLICA_IP"
 	    #curl -u $TOKEN  -IL $LOCATION/api/json?pretty=true
 	fi
 	# sleep
