6e9ca95b9afd8b1d69e03367c5ceb1ee7dcc98e5
cccaternberg <acaternberg@cloudbees.com>
2024-10-02 08:37:32 +0200

    update

--- test/testJobTrigger.sh
+++ test/testJobTrigger.sh
@@ -1,12 +1,25 @@
 #! /bin/bash
 
+# This script triggers the test pipeline on the controller and prints the response header
+
+
+#see curl headers: https://daniel.haxx.se/blog/2022/03/24/easier-header-picking-with-curl
+
 source ../env.sh
 
-#see curl headers https://daniel.haxx.se/blog/2022/03/24/easier-header-picking-with-curl/
-
-echo "YOU NEED TO CREATE A ADMIN TOKEN ON THE OPERATIONS CENTER FIRST"
-
 JENKINS_TOKEN=${1:-"YOUR_ADMIN_TOKEN"}
+if [ $# -eq 1 ]; then
+    JENKINS_TOKEN=$1
+    echo "Use token $1"
+else
+    echo "Script must be called with exactly one parameter."
+    echo "YOU NEED TO CREATE A ADMIN TOKEN ON THE OPERATIONS CENTER: create on here: http://${OC_URL}/user/admin/configure"
+    echo "Usage: $0 <CJOC_JENKINS_ADMIN_TOKEN>"
+    exit 1
+fi
+
+
+
 JENKINS_USER_TOKEN="admin:$JENKINS_TOKEN"
 #CONTROLLER_URL=http://client.ha
 CONTROLLER_URL=http://${CLIENTS_URL}
@@ -22,7 +35,16 @@ do
  	echo "#######################################"
 	echo "start build of Job: $JOB"
   # trigger our testpipeline
-	curl --connect-timeout  $CONNECT_TIMEOUT  -s -IL -o $RESPONSEHEADERS  -u $JENKINS_USER_TOKEN -X POST  "$CONTROLLER_URL/job/$JOB/build?delay=0sec"
+
+  COOKIE="cookies.txt"
+  # save the cookie : -c cookies.txt
+  # send the coolie: -b cookies.txt
+	curl --connect-timeout  $CONNECT_TIMEOUT \
+	 -v -s -IL -o $RESPONSEHEADERS  \
+	 -c $COOKIE \
+	 -u $JENKINS_USER_TOKEN -X POST \
+	 "$CONTROLLER_URL/job/$JOB/build?delay=0sec"
+
   # check if we got a healthy HTTP response state in the response header
   # Response header gets written by each loop/request in the $RESPONSEHEADERS (heade) file
   if [ -z "$(cat $RESPONSEHEADERS |grep -oE 'HTTP/2 201|HTTP/ 200|HTTP/1.1 201')" ]
@@ -39,7 +61,8 @@ do
 	    echo "CONTROLLER_REPLICA:          $REPLICA"
 	    echo "CONTROLLER_REPLICA_IP:       $REPLICA_IP"
 	    echo "CONTROLLER_STICKY_COOKIE:    $STICKY_COOKIE"
-	    #curl -u $TOKEN  -IL $LOCATION/api/json?pretty=true
+	    echo "COOKIE: $(cat cookies.txt | grep -oE ${CLIENTS_URL}.*$)"
+	    #curl -u $JENKINS_USER_TOKEN  -IL $CONTROLLER_URL/api/json?pretty=true
 	fi
 	sleep $CONNECT_TIMEOUT
 done
