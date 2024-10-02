ca2a2feac35b2e22c38d22957702830eeaf819d3
cccaternberg <acaternberg@cloudbees.com>
2024-10-02 08:28:16 +0200

    update

--- up.sh
+++ up.sh
@@ -22,7 +22,6 @@ else
 fi
 
 echo "#### Assign $SSH_PUBLIC_KEY_PATH to JENKINS_AGENT_SSH_PUBKEY"
-# THE FOLLOWING IS NOT VERY SECURED, AS LONG AS WE DO SO JUST ON LOCALHOST FOR DEMO PURPOSE IT SHOULD BE OK
 # Expose SSH PUP_KEY  to Agent authorized_key , see https://hub.docker.com/r/jenkins/ssh-agent for details
 export JENKINS_AGENT_SSH_PUBKEY=$(cat $SSH_PUBLIC_KEY_PATH)
 
