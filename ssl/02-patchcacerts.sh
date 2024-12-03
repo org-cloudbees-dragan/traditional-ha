3888b09866744b534bfea595d1e61b5bcf6435bf
cccaternberg <acaternberg@cloudbees.com>
2024-12-03 20:07:58 +0100

    coment

--- ssl/02-patchcacerts.sh
+++ ssl/02-patchcacerts.sh
@@ -2,5 +2,5 @@
 
 cp -v $JAVA_HOME/lib/security/cacerts .
 cat jenkins.crt jenkins.key > jenkins.pem
-keytool -delete -noprompt -alias jenkins -keystore cacerts -storepass changeit
+#keytool -delete -noprompt -alias jenkins -keystore cacerts -storepass changeit
 keytool -import -noprompt -keystore cacerts -file jenkins.pem -storepass changeit -alias jenkins;
