40949c8f1eede4c572f38829fd3e077f6d79a328
cccaternberg <acaternberg@cloudbees.com>
2024-12-08 11:34:56 +0100

    add tools check

--- ssl/01-createSelfSigned.sh
+++ ssl/01-createSelfSigned.sh
@@ -1,8 +1,19 @@
 #! /bin/bash
 
-DOMAIN="*.ha"
-#openssl req -x509 -sha256 -nodes -days 365 -newkey rsa:2048 -keyout ./privateKey.key -out ./certificate.crt \
-#             -subj "/C=DE/ST=Berlin/L=Berlin/O=Cloudbees/OU=PS/CN=$DOMAIN"
+if [[ -z "${JAVA_HOME}" ]]; then
+  echo "JAVA_HOME is not set. Set JAVA_HOME first"
+  exit 1
+else
+  echo "JAVA_HOME is set to: $JAVA_HOME"
+fi
+
+
+if command -v openssl >/dev/null 2>&1; then
+  echo "OpenSSL is installed."
+else
+  echo "OpenSSL is not installed."
+  exit 1
+fi
 
 
 # Generate a self-seigned TLS certificate, with a subject alternative name matching two additional hostnames:
